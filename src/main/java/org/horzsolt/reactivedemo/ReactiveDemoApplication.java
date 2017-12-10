e41aee92193359999f9b11f86f0d22afc2f8ad24
horzsolt <horzsolt2006@gmail.com>
2017-12-10 17:03:03 +0100

    Initial

---
+++ src/main/java/org/horzsolt/reactivedemo/ReactiveDemoApplication.java
@@ -0,0 +1,71 @@
+package org.horzsolt.reactivedemo;
+
+import lombok.*;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.stereotype.Component;
+import reactor.core.publisher.Flux;
+
+import java.util.Date;
+
+@SpringBootApplication
+public class ReactiveDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveDemoApplication.class, args);
+	}
+
+}
+
+@Component
+class SampleDataIntitializer implements ApplicationRunner {
+
+	private final MovieRepository mr;
+
+	public SampleDataIntitializer(MovieRepository mr) {
+		this.mr = mr;
+	}
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+
+
+		Flux<Movie> movieFlux = Flux.just("Silence of Lambdas", "Y to Mono Tumbien", "Back to the Future", "AEon Flux")
+				.map(m -> new Movie(m))
+				.flatMap(mr::save);
+
+		/*mr.deleteAll()
+				.thenMany(movieFlux)
+				.thenMany(mr.findAll())
+				.subscribe(System.out::println);*/
+
+		movieFlux.thenMany(mr.findAll()).subscribe(System.out::println);
+	}
+}
+
+
+interface MovieRepository extends ReactiveMongoRepository<Movie, String> {
+}
+
+@Document
+@Data
+@NoArgsConstructor
+@RequiredArgsConstructor
+class Movie {
+	private String id;
+	@NonNull
+	private String title;
+}
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+class MovieEvent {
+	private String movieId;
+	private Date date;
+}
